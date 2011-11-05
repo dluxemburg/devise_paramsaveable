a4e791966a950b7ad54ddf822e27adfcdde10845
Daniel Luxemburg <daniel.luxemburg@gmail.com>
2011-11-05 19:43:49 -0400

    working now

--- lib/devise_paramsaveable/hooks/paramsaveable.rb
+++ lib/devise_paramsaveable/hooks/paramsaveable.rb
@@ -1,12 +1,13 @@
 Warden::Manager.after_set_user do |user, auth, opts|
   if user.saveable_params.present?
-    request = Rack::Request.new(env)
+    request = Rack::Request.new(auth.env)
     user.saveable_params.each do |p|
-      if(request.params[p.name])
-        user[p.name] = request.params[p.name]
-      elsif p.clear_on_empty == true
-        user[p.name] = nil
+      if(request.params[p[:name].to_s].present?)
+        user[p[:name]] = request.params[p[:name].to_s]
+      elsif p[:clear_on_empty] == true
+        user[p[:name]] = nil
       end
     end
+    user.save()
   end
 end
