776146c8f8263220eca6577b0354b965323d0407
Daniel Luxemburg <daniel.luxemburg@gmail.com>
2011-11-14 09:52:12 -0500

    use strings not symbolds

--- lib/devise_paramsaveable/hooks/paramsaveable.rb
+++ lib/devise_paramsaveable/hooks/paramsaveable.rb
@@ -2,12 +2,13 @@ Warden::Manager.after_set_user do |user, auth, opts|
   if user.saveable_params.present?
     request = Rack::Request.new(auth.env)
     user.saveable_params.each do |p|
-      if(request.params[p[:name].to_s].present?)
-        user[p[:name]] = request.params[p[:name].to_s]
+      if(request.params[p[:name]].present?)
+        puts '[devise_paramsaveable] writing '+p[:name]+' attribute to be '+request.params[p[:name]]
+        user.write_attribute(p[:name],request.params[p[:name]])
       elsif p[:clear_on_empty] == true
-        user[p[:name]] = nil
+        user.write_attribute(p[:name],nil)
       end
     end
-    user.save()
+    user.save!
   end
 end
